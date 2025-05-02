4d4001387896c72ee13f7b94a0758f810bbf4fef
Ashish Karhade <akarhade5@gmail.com>
2025-05-02 08:45:51 +0530

    DB connection established

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/AshiishKarhade/url-shortner-go/pkg/database"
 	"github.com/AshiishKarhade/url-shortner-go/pkg/hashing"
 	"github.com/AshiishKarhade/url-shortner-go/pkg/snowflake"
 	"log"
 )
 
 func main() {
+
+	db, err := database.ConnectPostgres()
+	if err != nil {
+		log.Fatalf("Failed to connect to PostgreSQL: %v", err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatalf("Failed to close database connection: %v", err)
+		}
+	}(db)
+
 	sf, err := snowflake.NewSnowflake(1)
 	if err != nil {
 		log.Fatalf("Failed to initialize snowflake: %v", err)
--- pkg/database/postgresql.go
+++ pkg/database/postgresql.go
@@ -1,1 +1,52 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq" // PostgreSQL driver
+	"log"
+	"time"
+)
+
+type Config struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		Host:     "localhost",
+		Port:     5432,
+		Username: "postgres",
+		Password: "password",
+		DBName:   "url_shortner",
+		SSLMode:  "disable",
+	}
+}
+
+func ConnectPostgres() (*sql.DB, error) {
+	config := DefaultConfig()
+	return ConnectPostgresWithConfig(config)
+}
+
+func ConnectPostgresWithConfig(config *Config) (*sql.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		config.Host, config.Port, config.Username, config.Password, config.DBName, config.SSLMode)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to PostgreSQL: %w", err)
+	}
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+	log.Println("Successfully connected to PostgreSQL database")
+	return db, nil
+}
