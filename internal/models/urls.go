b89d4e7516d64d18db4cd7dc85306109c5664842
Ashish Karhade <akarhade5@gmail.com>
2025-05-02 08:28:44 +0530

    files add

---
+++ internal/models/urls.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type URL struct {
+	ID        int64     `json:"id"`
+	LongURL   string    `json:"long_url"`
+	ShortURL  string    `json:"short_url"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
